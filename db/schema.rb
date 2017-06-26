fcb60a133720ebf4ba83f6d6188fac7ed9d1e5a9
esimonian <eli.simonian@gmail.com>
2017-06-26 15:49:46 -0700

    add user id fields to contacts/lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626191038) do
+ActiveRecord::Schema.define(version: 20170626224915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20170626191038) do
     t.float "address_lng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "contacts_lists", force: :cascade do |t|
@@ -60,6 +62,8 @@ ActiveRecord::Schema.define(version: 20170626191038) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "templates", force: :cascade do |t|
@@ -98,5 +102,6 @@ ActiveRecord::Schema.define(version: 20170626191038) do
 
   add_foreign_key "campaigns", "templates"
   add_foreign_key "campaigns", "users"
+  add_foreign_key "contacts", "users"
   add_foreign_key "templates", "users"
 end
