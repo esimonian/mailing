cd7af9950b2fbdb6d5cbd5fe4ee8302ddae2f1e9
esimonian <eli.simonian@gmail.com>
2017-06-28 14:20:21 -0700

    heroku database fix

--- db/migrate/20170626185923_create_lists.rb
+++ db/migrate/20170626185923_create_lists.rb
@@ -2,6 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.1]
   def change
     create_table :lists do |t|
       t.string :title
+      t.reference :user, foreign_key: true
 
       t.timestamps
     end
